86301a50295a4ffb71d18801e3d5fffc850abbba
Sneha <Sneha@TYSS-Sneha.mshome.net>
2024-07-12 18:45:57 +0530

    Added BaseClass If else

--- Selenium_Project/src/main/java/BaseClassUtility/BaseClass.java
+++ Selenium_Project/src/main/java/BaseClassUtility/BaseClass.java
@@ -4,5 +4,14 @@ public class BaseClass {
 
 	public static void main(String[] args) {
 		System.out.println("hii sam");
+		if("HDFC".equals("HDFC"))
+		{
+			System.out.println("Pass");
+		}
+		else
+		{
+			System.out.println("Fail");
+		}
 	}
+		
 }
